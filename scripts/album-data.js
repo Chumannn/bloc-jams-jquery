3fc18f239c816d7af98f93ceda37e71e22c74d39
Chuck <chuckdushane@gmail.com>
2018-01-23 17:03:59 -0600

    section 16 assignment complete

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -14,5 +14,8 @@ const album = {
 
 {
   $('#album-title').text(album.title);
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $
 }
